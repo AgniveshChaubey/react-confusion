e26566f5aafc3d9d8d721c47f7d49401564447b4
Agnivesh Chaubey <agniveshvapi@gmail.com>
2022-11-17 01:26:50 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,3 +4,24 @@ import Home from './HomeComponent';
 
 
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+
+
+class Main extends Component {
+
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
+      };
+    }
+}
